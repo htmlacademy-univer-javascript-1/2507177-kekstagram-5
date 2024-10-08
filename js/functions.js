b7c2c3c16495a945330d4489c9aa4e181b9ee8b4
Анна Пивоварова <app0605@mail.ru>
2024-10-08 15:18:59 +0500

    функции для тренировки

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,14 @@
+const checkStringLength = function(str, maximumLength) {
+  return str.length <= maximumLength;
+}
 
-const stringLength = function(inputPhrase, maximumLength) {
-  if (inputPhrase.length <= maximumLength) {
-    return true;
-  } else {
-    return false;
+function isPalindrome(str) {
+  const normalizedStr = str.replaceAll(' ', '').toLowerCase();
+  let reversedStr = '';
+
+  for (let i = normalizedStr.length - 1; i >= 0; i--) {
+    reversedStr += normalizedStr[i];
   }
-};
 
-const palindrome = function(inputPhrase) {
-  let string = inputPhrase.replaceAll(' ', '');
-  string = string.toUpperCase();
-  let newPhrase = '';
-  for (let i = string.length - 1; i >= 0; i--) {
-    newPhrase += string[i];
-  }
-  return newPhrase === string;
-};
-
-function name(input) {
-
-  const str = input.toString();
-  let result = '';
-
-  for (let char of str) {
-
-    if (!Number.isNaN(parseInt(char))) {
-        result += char;
-      }
-    }
-
-    return result ? parseInt(result) : NaN;
+  return normalizedStr === reversedStr;
 }
