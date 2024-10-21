75596226a5579f836d719c59bfd2229d074c2da4
Keks <keks@htmlacademy.ru>
2024-10-22 00:41:29 +0300

    :heavy_check_mark: Сборка #20

---
+++ 20/js/functions.js
@@ -0,0 +1,14 @@
+function checkStringLength(str, maxLenath) {
+  return str.length <= maxLenath;
+}
+
+function isPalindrome(str) {
+  const normalizedStr = str.replaceAll(' ', '').toLowerCase();
+  let reversedStr = '';
+
+  for (let i = normalizedStr.length - 1; i >= 0; i--) {
+    reversedStr += normalizedStr[i];
+  }
+
+  return normalizedStr === reversedStr;
+}
