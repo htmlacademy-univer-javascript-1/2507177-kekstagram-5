453f1ff1a3ac5d446edf174bbd9de9aa66efea75
Анна Пивоварова <app0605@mail.ru>
2024-11-27 21:17:36 +0500

    Помощь друга

--- js/form.js
+++ js/form.js
@@ -1,3 +1,8 @@
+
+import { resetScale } from './scale.js';
+import { init, reset } from './effects.js';
+
+
 const MAX_COMMENT_LENGTH = 140;
 const MAX_HASHTAG_COUNT = 5;
 const VALID_HASHTAG = /^#[a-zа-яё0-9]{1,19}$/i;
@@ -78,6 +83,8 @@ const closeForm = () => {
   imgUploadOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
+  resetScale();
+  reset();
 };
 
 function onDocumentKeydown(evt) {
@@ -92,3 +99,4 @@ function onDocumentKeydown(evt) {
 imgUploadCancel.addEventListener('keydown', onDocumentKeydown);
 imgUploadCancel.addEventListener('click', closeForm);
 imgUploadForm.addEventListener('change', showForm);
+init();
